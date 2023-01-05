4f87c40a3ea1b498e757ba0711385547871198d0
Mateusz Jasiewicz <mat.jasiewicz@gmail.com>
2023-01-05 10:48:10 +0100

    Init commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Repository\UserProfileRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    private array $messages = [
+        ['message' => 'Hello', 'created' => '2022/12/12'],
+        ['message' => 'Hi', 'created' => '2022/10/12'],
+        ['message' => 'Bye', 'created' => '2021/05/12']
+    ];
+
+    #[Route('/', name: 'app_index')]
+    public function index(UserProfileRepository $profiles): Response
+    {
+
+        
+
+
+
+        // Dodawanie
+        // $user = new User();
+        // $user->setEmail('email@email.com');
+        // $user->setPassword('12345678');
+
+        // Edycja
+        // $profile = new UserProfile();
+        // $profile->setUser($user);
+        // $profiles->save($profile, true);
+
+        // Usuwanie
+        // $profile = $profiles->find(1);
+        // $profiles->remove($profile, true);
+
+        return $this->render('hello/index.html.twig', [
+            'messages' => $this->messages,
+            'limit' => 3
+        ]);
+    }
+
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+    public function showOne(int $id): Response
+    {
+        return $this->render('hello/show_one.html.twig', [
+            'message' => $this->messages[$id]
+        ]);
+    }
+}
